9bebfd1372bbe3d4fc5873d3e1e2aaef34e79c12
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-03-20 01:15:33 -0400

    Fix linting error.

--- packages/wp-build/lib/build.mjs
+++ packages/wp-build/lib/build.mjs
@@ -1733,6 +1733,7 @@ async function buildRoute( routeName ) {
 /**
  * Build all discovered routes.
  *
+ * @param  activePageIds
  * @return {Promise<void>}
  */
 async function buildAllRoutes( activePageIds = null ) {
